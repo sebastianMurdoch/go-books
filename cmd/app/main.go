255784d88ee63c419c29e0218ffee2cc94e2c72e
Sebastian Murdoch <sebastianmurdoch12@gmail.com>
2020-09-19 16:07:18 -0300

    add newrelic

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"net/http"
+)
+
+var newRelicApp *newrelic.Application
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", HandlerFunc)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+func init()  {
+	var err error
+	newRelicApp, err = newrelic.NewApplication(
+		newrelic.ConfigAppName("newRelicApp"),
+		newrelic.ConfigLicense("0cfe72a4df09c8d5723a142b8040134281b8NRAL"),
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func HandlerFunc(c *gin.Context)  {
+	txn := newRelicApp.StartTransaction("status")
+	defer txn.End()
+	c.String(http.StatusOK, "pong")
+}
+
